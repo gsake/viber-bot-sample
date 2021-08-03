4a252e57641b905a88bd86e0a0a4bd20110ece1e
georgios sakellaris <georgios.sakellaris@atos.net>
2021-08-03 13:25:59 +0300

    Make it simpe

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const config = require('./config.json');
-
-const TOKEN = config.token;
-const WEBHOOK_URL = config.expose_domain + config.expose_uri_path;
-const PORT = config.port;
-
-
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const TextMessage = require('viber-bot').Message.Text;
-
+const winston = require('winston');
 const express = require('express'); 
+
 const app = express();
-const logger = require('./logger');
+const config = require('./config.json');
+
+
+let logger = winston.createLogger({
+    exitOnError: false,
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'app.log'})
+    ]
+})
+
 
 const bot = new ViberBot({
 	logger: logger,
-	authToken: TOKEN,
+	authToken: config.token,
 	name: config.name,
 	avatar: config.avatar
 });
 
 bot.on(BotEvents.SUBSCRIBED, response => {
-
 	response.send(new TextMessage(`Hi ${response.userProfile.name}, my name is ${bot.name}!`));
 });
 
@@ -31,12 +34,12 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
 
 app.use(config.expose_uri_path, bot.middleware());
 
-app.listen(PORT, () => {
+app.listen(config.port, () => {
 
-	logger.info(`Application is running! Port: ${PORT}`);
+	logger.info(`Application is running! Port: ${config.port}`);
 
-	bot.setWebhook(WEBHOOK_URL).catch(error => {
-		logger.debug(`Error: The webhook ${WEBHOOK_URL} cannot be set. ${error}`);
+	bot.setWebhook(config.expose_domain + config.expose_uri_path).catch(error => {
+		logger.debug(`Error: The webhook ${config.expose_domain + config.expose_uri_path} cannot be set. ${error}`);
 		process.exit(1);
 	});
 
